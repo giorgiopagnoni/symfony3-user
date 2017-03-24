b7f37199c275fd30311c5dafd49c4fbcb97f3748
Giorgio Pagnoni <giorgio.pagnoni@sintattica.it>
2017-03-24 15:41:51 +0100

    assetic test

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Gregwar\CaptchaBundle\GregwarCaptchaBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new AppBundle\AppBundle()
         ];
 
